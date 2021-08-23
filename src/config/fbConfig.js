122b544bc18d1b9d20f9de0e4c9f7da58a4191e9
Roshin <theroshin@gmail.com>
2021-08-22 19:55:53 -0500

    test

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,14 @@ import 'firebase/auth';
 require('dotenv').config()
 
 var config = {
-    apiKey: process.env.MY_SECRET_KEY,
+    apiKey: "AIzaSyBEC7xcBGF96IWGlLke3e51Xkx1FmXFubg",
     authDomain: "torqata-dashboard.firebaseapp.com",
     projectId: "torqata-dashboard",
     storageBucket: "torqata-dashboard.appspot.com",
-    messagingSenderId: process.env.SENDER_ID,
-    appId: process.env.APP_ID
+    messagingSenderId: "37537403062",
+    appId: "1:37537403062:web:bd371bbb171abe74979617"
 };
+
 
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
